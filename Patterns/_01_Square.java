d7c3bc3e76b3ba7075edadebd3c3dd135f2bace2
ATUL <atulmint1502@gmail.com>
2025-12-29 21:22:57 +0530

    Another pattern

--- Patterns/_01_Square.java
+++ Patterns/_01_Square.java
@@ -21,7 +21,7 @@ public class _01_Square {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
        
-        //   score1(n);
+        score1(n);
             //   score2(n);
             //score3(n);
      
